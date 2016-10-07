88314ed6d1d8755e634ce862ecf62349a2547e8e
Anatoly Yakovenko <aeyakovenko@gmail.com>
2016-10-07 16:19:29 -0700

    initial monitor

---
+++ src/tcp_metrics_monitor.erl
@@ -0,0 +1,89 @@
+%%%-------------------------------------------------------------------
+%%% @author Anatoly Yakovenko
+%%% @copyright (C) 2015, Mesosphere
+%%% @doc
+%%%
+%%% @end
+%%% Created : 03. Oct 2016 11:44 AM
+%%%-------------------------------------------------------------------
+-module(tcp_metrics_monitor).
+-author("Anatoly Yakovenko").
+
+-behaviour(gen_server).
+
+-export([start_link/0]).
+
+-export([init/1,
+    handle_call/3,
+    handle_cast/2,
+    handle_info/2,
+    terminate/2,
+    code_change/3]).
+
+-define(SERVER, ?MODULE).
+
+-record(state, {
+    }).
+
+-spec(start_link() ->
+    {ok, Pid :: pid()} | ignore | {error, Reason :: term()}).
+start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+-spec(init(term()) ->
+    {ok, State :: #state{}} | {ok, State :: #state{}, timeout() | hibernate} |
+    {stop, Reason :: term()} | ignore).
+init([]) ->
+    process_flag(trap_exit, true),
+    erlang:send_after(splay_ms(), self(), poll_proc),
+    {ok, #state{}}.
+
+-spec(handle_call(Request :: term(), From :: {pid(), Tag :: term()},
+    State :: #state{}) ->
+    {reply, Reply :: term(), NewState :: #state{}} |
+    {reply, Reply :: term(), NewState :: #state{}, timeout() | hibernate} |
+    {noreply, NewState :: #state{}} |
+    {noreply, NewState :: #state{}, timeout() | hibernate} |
+    {stop, Reason :: term(), Reply :: term(), NewState :: #state{}} |
+    {stop, Reason :: term(), NewState :: #state{}}).
+handle_call(_Request, _From, State) ->
+    {reply, ok, State}.
+
+-spec(handle_cast(Request :: term(), State :: #state{}) ->
+    {noreply, NewState :: #state{}} |
+    {noreply, NewState :: #state{}, timeout() | hibernate} |
+    {stop, Reason :: term(), NewState :: #state{}}).
+handle_cast(_Request, State) ->
+    {noreply, State}.
+
+-spec(handle_info(Info :: timeout() | term(), State :: #state{}) ->
+    {noreply, NewState :: #state{}} |
+    {noreply, NewState :: #state{}, timeout() | hibernate} |
+    {stop, Reason :: term(), NewState :: #state{}}).
+handle_info(poll_proc, State) ->
+    ProcFile = ip_vs_conn_config:proc_file(),
+    New = ip_vs_conn:fold(
+              fun(Conn, Map) -> 
+                      ip_vs_conn_map:update(State#state.syns, Conn, Map)
+              end,
+              maps:new(),
+              ProcFile),
+    NewState = State#state{syns = New},
+    erlang:send_after(splay_ms(), self(), poll_proc),
+    {noreply, NewState};
+   
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+-spec(terminate(Reason :: (normal | shutdown | {shutdown, term()} | term()),
+    State :: #state{}) -> term()).
+terminate(_Reason, _State = #state{}) ->
+    ok.
+
+-spec(code_change(OldVsn :: term() | {down, term()}, State :: #state{},
+    Extra :: term()) ->
+    {ok, NewState :: #state{}} | {error, Reason :: term()}).
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+
